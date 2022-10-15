37e89201840ce8e130eeb3c878cbefdf47dcdd55
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-10-15 18:18:14 +0530

    Add project files.

---
+++ Data/TraineeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using CodeFirst.Models;
+using System.Collections.Generic;
+
+namespace CodeFirst.Data
+{
+    public class TraineeDbContext : DbContext
+    {
+        public TraineeDbContext(DbContextOptions<TraineeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Trainee> Trainees { get; set; }
+
+    }
+}
